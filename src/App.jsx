fb075431758fb7847009067c6cba034ffafb4e24
Jeda@Win <salimbjed21@gmail.com>
2026-05-21 00:03:52 +0800

    5th commit, manage supabase and deploy in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import { supabase } from "./supabase/client";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes((prev) => {
-      return [...prev, newNote];
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function addNote(newNote) {
+    const { date, error } = await supabase.from("notes").insert({
+      title: newNote.title,
+      content: newNote.content,
     });
+
+    if (error) {
+      console.log(error);
+    } else {
+      fetchNotes();
+    }
   }
 
-  function deleteNote(id) {
-    setNotes((prev) => {
-      return prev.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
+  async function fetchNotes() {
+    const { data, error } = await supabase.from("notes").select("*");
+
+    if (error) {
+      console.log(error);
+    } else {
+      setNotes(data);
+    }
+  }
+
+  async function deleteNote(id) {
+    const { data, error } = await supabase.from("notes").delete().eq("id", id);
+
+    if (error) {
+      console.log(error);
+    } else {
+      fetchNotes();
+    }
   }
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
+      {notes.map((noteItem) => {
         return (
           <Note
-            key={index}
-            id={index}
+            key={noteItem.id}
+            id={noteItem.id}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
           />
         );
       })}
-
       <Footer />
     </div>
   );
